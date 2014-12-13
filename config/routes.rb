edd0e771a154f0bb57802c426560d2df7bb7210e
seairy <seairy@gmail.com>
2014-12-13 21:22:52 +0800

    bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   scope 'markets' do
     get '/', to: 'markets#index', as: :markets
   end
-  resources :products do
-    get :test, on: :collection
+  scope 'products', as: 'products' do
+    get 'car_mobile_communication_antenna_point_6', to: 'products#car_mobile_communication_antenna_point_6', as: :car_mobile_communication_antenna_point_6
+    get 'car_mobile_communication_antenna_point_9', to: 'products#car_mobile_communication_antenna_point_9', as: :car_mobile_communication_antenna_point_9
+    get 'portable_transportable_satellite_station', to: 'products#portable_transportable_satellite_station', as: :portable_transportable_satellite_station
+    get 'shipboard_antenna_c', to: 'products#shipboard_antenna_c', as: :shipboard_antenna_c
+    get 'shipboard_antenna_ku', to: 'products#shipboard_antenna_ku', as: :shipboard_antenna_ku
+    get 'through_static_antenna', to: 'products#through_static_antenna', as: :through_static_antenna
   end
 end
