89c5c893572fbccfb033a7f06d2867feb57f2af7
seairy <seairy@gmail.com>
2015-05-06 22:05:47 +0800

    init commit

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,3 @@ server '60.247.115.253', port: 3030, user: 'root', roles: %w{web app db}, primar
 
 set :deploy_to, "/srv/www/Begonia"
 set :rails_env, :production
-
-set :config_files, %w(
-  abc
-)
